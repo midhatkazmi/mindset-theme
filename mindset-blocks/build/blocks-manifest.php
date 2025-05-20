5f293928465f3ac093d9c8c8bcd2b66eaa878418
Midhat <mkazmi2@my.bcit.ca>
2025-05-20 09:54:37 -0700

    Day 6 afternoon task 2 complete

--- mindset-blocks/build/blocks-manifest.php
+++ mindset-blocks/build/blocks-manifest.php
@@ -86,5 +86,24 @@ return array(
 		'editorScript' => 'file:./index.js',
 		'style' => 'file:./style-index.css',
 		'render' => 'file:./render.php'
+	),
+	'service-posts' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'mindset-blocks/service-posts',
+		'version' => '1.0.0',
+		'title' => 'Service Posts',
+		'category' => 'widgets',
+		'icon' => 'forms',
+		'description' => 'A block to display the Service posts with in-page navigation at the top',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'service-posts',
+		'editorScript' => 'file:./index.js',
+		'style' => 'file:./style-index.css'
 	)
 );
